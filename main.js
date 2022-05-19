726dce25037abe5465c6f6e45a8b270542ca85e8
Toch <tochjob@gmail.com>
2022-05-19 15:45:04 +0300

    Обучение иоё

---
+++ main.js
@@ -0,0 +1,27 @@
+const goods = [
+    { title: 'Shirt', price : 50},
+    { title: 'T-Shirt', price : 100},
+    { title: 'Some', price : 150},
+    { title: 'Socks', price : 200},
+    { title: 'Shoes', price : 250},
+]
+
+const renderGoodsItem = (title = 'Item title', price='Item price') => {
+        return `
+        <div class="goods-item">
+            <img src="" alt=""> 
+            <h3>${title}</h3>
+            <p>${price}</p>
+        </div>
+    `
+}
+
+const renderGoodsList = ( [...rest] ) => {
+    let goodsList = rest.map(({title, price} = item) => {
+        return renderGoodsItem(title, price)        
+    }).join('')
+    let box = document.querySelector('.goods')
+    box.innerHTML = goodsList
+}
+
+renderGoodsList(goods)
