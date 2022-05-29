c5bbb7ff6c472bfe30ad900fa69d3c688c789e38
Toch <tochjob@gmail.com>
2022-05-29 10:44:09 +0300

    homework_3

--- main.js
+++ main.js
@@ -6,9 +6,25 @@ const goods = [
     { title: 'Shoes', price : 250},
 ]
 
+const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+const GOODS = `${BASE_URL}/catalogData.json`
+const BASKET_GOODS = `${BASE_URL}/getBasket.json`
+
+
+function service(url, callback){
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url);
+    const loadHandler = () => {
+        callback(JSON.parse(xhr.response))
+    }
+    xhr.onload = loadHandler
+
+    xhr.send();
+}
+
 class GoodsItem {
-    constructor({title = 'Item title', price='Item price'}){
-        this.title = title;
+    constructor({product_name = 'Item title', price='Item price'}){
+        this.title = product_name;
         this.price = price;
     }
     render () {
@@ -23,9 +39,12 @@ class GoodsItem {
 }
 
 class GoodsList {
-
-    fetchData(){
-        this.list = goods;
+    items = []
+    fetchGoods(callback){
+        service(GOODS, (data)=>{
+            this.list = data;
+            callback()
+        })
     }
     getCount(){
         this.list.reduce((sum, {price}) => sum + price, 0);
@@ -40,10 +59,26 @@ class GoodsList {
     }
 }
 
+class BasketGoodsList{
+    items = [];
+    fetchData(callback){
+        service(BASKET_GOODS, (data)=>{
+            this.items = data;
+            callback()
+        })
+    }
+}
+
 
 
 
 const goodsList = new GoodsList(goods)
-goodsList.fetchData()
-goodsList.getCount()
-goodsList.render()
+goodsList.fetchGoods(()=>{
+    goodsList.getCount()
+    goodsList.render()
+})
+
+const basketGoods= new BasketGoodsList();
+basketGoods.fetchData(()=>{
+    
+})
